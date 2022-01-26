3a4ec637b401fff90f755c378dd9c2542d059464
Jose Pulido <al_jpulidod@iessierradelasvillas.org>
2022-01-26 12:45:43 +0100

    Try - Except, Juego números aleatorios

---
+++ Ejercicio(Aleatorio).py
@@ -0,0 +1,42 @@
+#Inicio Juego: Adivina el número aleatorio (Ejercicios de refuerzo)
+
+#Librerias
+import random
+
+#Inicialización variables necesarias
+intentos = 1
+n = random.randint(0,10)
+
+#Introducción de variables correctas
+bandera = True
+while bandera:
+
+    try:
+        num = int(input("Escribe un número entre 1 y 10: "))
+        bandera = False
+
+    except:
+        print("ES OBLIGATORIO ESCRIBIR UN NÚMERO")
+        intentos = intentos + 1
+
+#Números mayor o menor al introducido
+while num != n:
+
+    if num < n:
+        print("El número introducido es menor")
+        num = int(input("Introduzca otro número: "))
+        intentos = intentos + 1
+    else:
+        print("El número introducido es mayor")
+        num = int(input("Introduzca otro número: "))
+        intentos = intentos + 1
+
+#Fin del juego
+if n == num:
+    print("Felicidades, has ganado el juego")
+    print("Ha necesitado un total de", intentos, "intentos")
+
+#Fin del programa
+
+
+    
---
+++ Try - Except.py
@@ -0,0 +1,6 @@
+try:
+    num = input("Escribe un número: ")
+    print(num + 5)
+except:
+    print("El número es una cadena de caracteres")
+
